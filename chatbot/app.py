11604df10d3e0f72350183bd12959ab04ba6c901
Swarnashruti Jupudi <swarna.s92@gmail.com>
2023-11-24 18:30:45 -0800

    Added state, role and print conversation history

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 st.markdown('# Chatbot')
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    st.markdown(message)
+
 user_prompt = st.chat_input()
+
 if user_prompt: 
-    response = f"echo: {user_prompt}"
+    st.markdown(f"user: {user_prompt}")
+    st.session_state.messages.append(f"user: {user_prompt}")
+    response = f"robot: {user_prompt}"    
     st.markdown(response)
+    st.session_state.messages.append(response)
+
