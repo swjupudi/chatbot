8fac90c386afdf8f468d03468ea35263f59b1217
Swarnashruti Jupudi <swarna.s92@gmail.com>
2023-11-25 13:27:05 -0800

    modified to show role icons and chat boxes

--- chatbot/app.py
+++ chatbot/app.py
@@ -6,14 +6,18 @@ if 'messages' not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    st.markdown(message)
+    name = 'user' if message['role'] == 'user' else 'assistant'
+    with st.chat_message(name):
+        st.markdown(message['content'])
 
 user_prompt = st.chat_input()
 
 if user_prompt: 
-    st.markdown(f"user: {user_prompt}")
+    with st.chat_message('user'):
+        st.markdown(user_prompt)
     st.session_state.messages.append({'role':'user', 'content': user_prompt})
-    response = f"robot: {converse(st.session_state.messages)}"   
-    st.markdown(response)
+    response = converse(st.session_state.messages) 
+    with st.chat_message("assistant"):
+        st.markdown(response)
     st.session_state.messages.append({'role':'assistant', 'content': response})
 
